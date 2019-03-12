4aba011f6560bdc8ca3b33a7c88160240efdea1c
Luis Gonzalez <louis1001@louis-mac.local>
2019-03-12 14:58:07 -0600

    Initial Commit.

---
+++ animations.js
@@ -0,0 +1,23 @@
+const nameText = "SoBadUI"
+
+
+function prepareAnimations(){
+	const compName = document.getElementById('comp-name')
+	
+	let nameAdvance = 0
+	function addLetter(){
+
+		compName.textContent = nameText.slice(0, nameAdvance) + "_"
+
+		if(nameAdvance != nameText.length){
+			nameAdvance++
+			setTimeout(addLetter, 150)
+		}else{
+			compName.textContent = nameText
+			compName.innerHTML += "<blink>|</blink>"
+		}
+	}
+	setTimeout(addLetter, 1000)
+}
+
+addEventListener('load', prepareAnimations)
